4a187b644ce0ea3ccf64322715d8f33a3e1c0a57
Duan Wei <duanwei@oppo.com>
2021-12-22 09:53:12 +0800

    add RequestContextResolverFactory

---
+++ restlight-core/src/main/java/io/esastack/restlight/core/resolver/param/RequestContextResolverFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 OPPO ESA Stack Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.esastack.restlight.core.resolver.param;
+
+import io.esastack.restlight.core.method.Param;
+import io.esastack.restlight.core.resolver.ParamResolver;
+import io.esastack.restlight.core.resolver.ParamResolverFactory;
+import io.esastack.restlight.core.serialize.HttpRequestSerializer;
+import io.esastack.restlight.core.util.Ordered;
+import io.esastack.restlight.server.context.RequestContext;
+
+import java.util.List;
+
+public class RequestContextResolverFactory implements ParamResolverFactory {
+
+    @Override
+    public ParamResolver createResolver(Param param, List<? extends HttpRequestSerializer> serializers) {
+        return (p, context) -> context;
+    }
+
+    @Override
+    public boolean supports(Param param) {
+        return RequestContext.class.isAssignableFrom(param.type());
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
+
